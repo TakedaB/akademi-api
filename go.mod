337a9899f8e59b459a8d8075d6deb9e5f3ef2bc2
TakedaB <contatobrunotakeda@gmail.com>
2026-08-25 15:09:38 -0300

    chore: setup inicial do projeto(go.mod, schema.sql, estrutura de pastas)

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/TakedaB/akademi-api
+
+go 1.26.5
---
+++ schema.sql
@@ -0,0 +1,28 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    role TEXT NOT NULL CHECK (role IN ('diretoria','financeiro','professor', 'aluno')),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE students (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    enrollment_number TEXT NOT NULL UNIQUE,
+    name TEXT NOT NULL,
+    birth_date DATE NOT NULL,
+    parent_name TEXT NOT NULL,
+    city TEXT,
+    phone TEXT NOT NULL,
+    email TEXT,
+    grade TEXT,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE INDEX idx_students_enrollment_number ON students (enrollment_number);
+CREATE INDEX idx_users_email ON users(email);    
